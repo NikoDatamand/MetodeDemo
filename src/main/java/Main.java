4b9ba9402b77fbdd6328a6f6632c69248212375d
NikoDatamand <niko081b@stud.kea.dk>
2022-09-12 10:11:06 +0200

    0.3 FLERE METODER

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         Person p1 = new Person(24, 1.76);
+
+        System.out.println(p1.getAlder() + " år " + p1.getHøjde() + " m ");
+        p1.setAlder(25);
+        p1.setHøjde(1.78);
+        System.out.println(p1.getAlder() + " år " + p1.getHøjde() + " m ");
+
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -7,4 +7,18 @@ public class Person {
         this.alder = alder;
         this.højde = højde;
     }
+
+    public int getAlder() {
+        return alder;
+    }
+    public double getHøjde() {
+        return højde;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+    public void setHøjde(double højde) {
+        this.højde = højde;
+    }
 }
